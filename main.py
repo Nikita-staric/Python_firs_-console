bc8a29f69c483c6f29775d604d548eda6f7f2e79
Nikita-staric <moses.nikitos@gmail.com>
2022-01-01 23:57:32 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from builtins import print
+
+numeric_variable=1212
+numeric_variable2=232
+ss=numeric_variable+numeric_variable2
+print(f"Тут ваше число>{ss}")
+array=["Nikita","Vasy","Peter","David","Dima",]
+number=1
+for new_array in array:
+ print(f"{ number}-Вау-->{new_array}-как ты")
+ number+=1
+#print("Car")
+webinar={
+"engine": "3.45",
+"weight": "299990",
+"Color": "Белый",
+"Speedometer": "350",
+"Speed": "300"
+}
+if numeric_variable>1000:
+ print("Бингоо");
+
+#функция
+def print_messege(msg):
+  print(msg["Speed"])
+  print_messege(webinar)
+#Комент
+#for new_array2 in webinar:
+# print(f"{new_array2}")
+
+'''
+if numeric_variable>400:
+  print("Все номанооо")
+else:
+   print("Надо навести суету")
+'''
